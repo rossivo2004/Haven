214a6c24addbf354358dd2d2f52370108aae2b90
Thanh Tai <duongthanhtai16042004@gmail.com>
2024-08-29 10:22:59 +0700

    'Body_Bodycart'

---
+++ frontend/src/app/cart/page.tsx
@@ -0,0 +1,10 @@
+import Body_Cart from "@/components/Body_Cart";
+import React from 'react';
+const CartPage = () => {
+    return (
+      <div>
+        <Body_Cart />
+      </div>
+    );
+  };
+export default CartPage;
