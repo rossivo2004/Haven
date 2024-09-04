add759c4cfa836364633a98d45f421a950fe85c8
Thanh Tai <duongthanhtai16042004@gmail.com>
2024-09-04 13:39:09 +0700

    Remove frontend directory

--- frontend/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Loader from "@/components/ui/Snippet";
-
-export default function Home() {
-  return (
-    <div className="max-w-screen-xl mx-auto px-4]">
-      aaaa
-    </div>
-  );
-}
--- frontend/src/app/shop/page.tsx
+++
@@ -1,9 +0,0 @@
-function Shop_Page() {
-    return ( 
-        <div className="">
-            aaaaSSS
-        </div>
-     );
-}
-
-export default Shop_Page;
