aab8fa65bf0d695a5ebcdad2ea6ade462adcc460
rossivo2004 <rossivo2004@gmail.com>
2024-09-16 11:34:34 +0700

    up

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,19 +2,18 @@ import "../src/styles/globals.css";
 import { Metadata, Viewport } from "next";
 import { Link } from "@nextui-org/link";
 import clsx from "clsx";
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 import { Providers } from "./providers";
-
 import Header from "@/src/components/Header";
 import Footer from "@/src/components/Footer";
 import BtnToTop from "@/src/components/BtnToTop";
-
 import { StoreProvider } from "@/src/store/StoreProvider";
-
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+
+import Script from 'next/script';  // Thêm import Script từ Next.js
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -35,20 +34,36 @@ export default function RootLayout({
 }) {
   return (
     <StoreProvider>
-    <html suppressHydrationWarning lang="en">
-      <head />
-      <body>
-        <Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
-         <Header />
-         <ToastContainer className='mt-18'/>
-         <BtnToTop />
-              {children}
-              <Analytics />
-              <SpeedInsights />
-           <Footer />
-        </Providers>
-      </body>
-    </html>
+      <html suppressHydrationWarning lang="en">
+        <head>
+          {/* Google Tag Manager */}
+          <Script id="google-tag-manager" strategy="afterInteractive">
+            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5MQ3DL62');`}
+          </Script>
+        </head>
+        
+        <body>
+          {/* Google Tag Manager (noscript) */}
+          <noscript>
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5MQ3DL62"
+              height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
+          </noscript>
+
+          <Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
+            <Header />
+            <ToastContainer className="mt-18" />
+            <BtnToTop />
+            {children}
+            <Analytics />
+            <SpeedInsights />
+            <Footer />
+          </Providers>
+        </body>
+      </html>
     </StoreProvider>
   );
 }
