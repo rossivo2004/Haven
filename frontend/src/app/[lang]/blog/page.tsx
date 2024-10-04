f917e71401e447ad9fd7b67985a0fe9e1b5f0d58
Linh <quanglinhle2102@gmail.com>
2024-10-04 16:17:11 +0700

    blog

--- frontend/src/app/[lang]/blog/page.tsx
+++ frontend/src/app/[lang]/blog/page.tsx
@@ -1,9 +1,24 @@
-import { title } from "@/src/components/primitives";
+// import { title } from "@/src/components/primitives";
 
-export default function BlogPage() {
+// export default function BlogPage() {
+//   return (
+//     <div>
+//       <h1 className={title()}>Blog</h1>
+//     </div>
+//   );
+// }
+
+import BlogDetail from '@/src/components/BlogDetail/BlogDetail';
+import BodyBlog from '@/src/components/BodyBlog/BodyBlog';
+
+
+
+const BlogPage = () => {
   return (
     <div>
-      <h1 className={title()}>Blog</h1>
+      <BodyBlog />
     </div>
   );
-}
+};
+
+export default BlogPage;
