efb57623b9be2bfdbe9952cfff48014999458b2f
rossivo2004 <rossivo2004@gmail.com>
2024-11-22 16:46:58 +0700

    up

--- admin/next.config.mjs
+++ admin/next.config.mjs
@@ -1,23 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['res.cloudinary.com', 'foodhavenadmin.vercel.app'],
+  images: {
+    domains: ['res.cloudinary.com', 'foodhavenadmin.vercel.app'],
+  },
+  reactStrictMode: false,
+  async rewrites() {
+    return [
+      {
+        source: '/signin',
+        destination: 'https://foodhavenadmin.vercel.app/signin',
       },
-      reactStrictMode: false,
-      async rewrites() {
-        return {
-          fallback: [
-            {
-              source: '/signin',
-              destination: 'https://foodhavenadmin.vercel.app/signin',
-            },
-            {
-              source: '/admin',
-              destination: 'https://foodhavenadmin.vercel.app/admin',
-            },
-          ],
-        }
+      {
+        source: '/admin',
+        destination: 'https://foodhavenadmin.vercel.app/admin',
       },
-}
+    ];
+  },
+};
 
 export default nextConfig;
