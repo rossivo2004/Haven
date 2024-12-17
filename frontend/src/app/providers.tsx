e5b7b1cf226041d316f6b74e6ef7cf568146f7b2
rossivo2004 <rossivo2004@gmail.com>
2024-12-17 12:24:42 +0700

    up

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -4,7 +4,7 @@ import * as React from "react";
 import { NextUIProvider } from "@nextui-org/system";
 import { useRouter } from "next/navigation";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { ThemeProviderProps } from "next-themes/dist/types";
+import type { ThemeProviderProps } from "next-themes";
 
 export interface ProvidersProps {
   children: React.ReactNode;
